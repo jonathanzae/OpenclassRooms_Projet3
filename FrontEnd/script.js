b090ede553898d955e535076dcbf23fea9d48132
Jonathan <jonathan.zaepfel@gmail.com>
2023-03-29 15:17:11 +0200

    Modifications 29/03/2023 15:17

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,37 +2,45 @@
 const reponse = await fetch("http://localhost:5678/api/categories");
 const categories = await reponse.json();
 
+// console.log(categories);
+
+let button1 = document.createElement("button");
+button1.innerText;
+button1.setAttribute("data-id", categories);
+document.getElementById("filters").appendChild(button1);
+
 // on creer une boucle pour parcourir le tableau
 for (let categorie of categories) {
-  // on cherche name dans le tableau categorie 
+  // on cherche name dans le tableau categorie
   let categorieName = categorie.name;
-  
+
   // on creer un boutton dans HTML
-  // grace a createElement 
+  // grace a createElement
   let button = document.createElement("button");
   // sur le boutton on associe name du tableau categorie
   button.innerText = categorieName;
-  // sur boutton on associe data-id (html) a l'id dans le tableau categorie
+  // setAttibute vas creer "data-id" et l'id des elements dans le tableau
   button.setAttribute("data-id", categorie.id);
   // on cherche l'id fileters dans HTML et on y associe l'element buttton creer
   document.getElementById("filters").appendChild(button);
-
 
   // on ajoute un click sur "button"
   // on y ajoute une fonction et en parametre un evenement
   button.addEventListener("click", function (event) {
     // on cible avec currentTarget sur chaques evenements click sur bouton
     let target = event.currentTarget;
+    // on appel a l'evenement click l'id du tableau creer grace setAttribute
+    //
     let categorieId = target.getAttribute("data-id");
-    
-    // la fonction filter : La méthode filter() sur tableau crée un nouveau tableau avec des éléments 
+
+    // la fonction filter : La méthode filter() sur tableau crée un nouveau tableau avec des éléments
     // qui répondent à un critère donné à partir d’un tableau existant
-    
-    // sur le tableau works on filtre chaque elements work categoryId 
+
+    // sur le tableau works on filtre chaque elements work categoryId
     // on associe a chaque boutton son tableau correspondant avec le contenu recuperé
-    // puis on affiche ce contenu 
+    // puis on affiche ce contenu
     let worksFiltered = works.filter(function (work) {
-      if(work.categoryId == categorieId){
+      if (work.categoryId == categorieId) {
         return work;
       }
     });
@@ -47,13 +55,12 @@ for (let work of works) {
   let worksProjects = work.imageUrl;
   let figcaption = work.title;
   // let figureImg = work.;
-  
-  // console.log(works);
- 
 
-  // let figure = document.createElement("figure");
-  // figure.innerHTML = figureImg;
-  // document.getElementById("gallery").appendChild(figure);
+  // console.log(works);
+
+  let figure = document.createElement("figure");
+  figure.innerHTML;
+  document.getElementById("gallery").appendChild(figure);
 
   let project = document.createElement("img");
   project.src = worksProjects;
@@ -62,7 +69,4 @@ for (let work of works) {
   let figcaptionImg = document.createElement("figcaption");
   figcaptionImg.innerHTML = figcaption;
   document.getElementById("gallery").appendChild(figcaptionImg);
-  
-
-  
 }
